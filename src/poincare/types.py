355cc26fbd00eb721f1307cb6fc38c0c857af62e
Mauro Silberberg <maurosilber@gmail.com>
2023-06-13 21:12:54 -0300

    Types

--- src/poincare/types.py
+++ src/poincare/types.py
@@ -1,15 +1,18 @@
 from __future__ import annotations
 
-from typing_extensions import dataclass_transform
+from typing_extensions import Self, dataclass_transform
 
 
 class Variable:
     def __init__(self, *, default: float | None = None):
         # dataclass_transform expects a `default` kw-only parameter.
-        raise NotImplementedError
+        self.default = default
 
     def derive(self, *, default: float | None = None) -> Derivative:
-        raise NotImplementedError
+        return Derivative(self, default=default, order=1)
+
+    def integral(self, *, default: float | None = None) -> Derivative:
+        return Derivative(self, default=default, order=-1)
 
     def __set__(self, obj, value: Variable | float):
         # Allows to override the annotation in System.__init__.
@@ -22,10 +25,29 @@ class Variable:
         raise NotImplementedError
 
 
-class Derivative(Variable):
-    def __init__(self, variable: Variable, *, default: float | None = None):
+class Derivative:
+    def __init__(
+        self,
+        variable: Variable,
+        *,
+        default: float | None = None,
+        order: int,
+    ):
         self.variable = variable
+        self.order = order
         self.default = default
+
+    def derive(self, *, default: float | None = None) -> Derivative:
+        return Derivative(self.variable, default=default, order=self.order + 1)
+
+    def integral(self, *, default: float | None = None) -> Derivative:
+        return Derivative(self.variable, default=default, order=self.order + -1)
+
+    def __eq__(self, other: Self):
+        if other.__class__ is not self.__class__:
+            return NotImplemented
+
+        return self.order == other.order and self.variable == other.variable
 
 
 @dataclass_transform(field_specifiers=(Variable,))
