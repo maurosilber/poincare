79a1117ba4736fce5a7b8a2cd436abe37043a36b
Mauro Silberberg <maurosilber@gmail.com>
2023-07-05 15:20:25 -0300

    Export __init__

---
+++ src/poincare/__init__.py
@@ -0,0 +1,10 @@
+from .types import Constant, Derivative, System, Variable, assign, initial
+
+__all__ = [
+    "Constant",
+    "Derivative",
+    "System",
+    "Variable",
+    "assign",
+    "initial",
+]
