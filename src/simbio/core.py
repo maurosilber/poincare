a917557e42321e7d368489a90fc943a2f452025b
Mauro Silberberg <maurosilber@gmail.com>
2023-07-16 01:17:22 -0300

    Update to Owned subclass

--- src/simbio/core.py
+++ src/simbio/core.py
@@ -5,67 +5,18 @@ from dataclasses import dataclass
 from typing import Callable, Iterator, Self, Sequence
 
 from poincare import Constant, Variable
-from poincare.types import Equation, Owned
+from poincare.types import Equation, Owned, System
 from symbolite import Symbol
 from symbolite.abstract.symbol import BinaryFunction
 
 
-class Reaction(Owned):
-    reactants: Sequence[Species]
-    products: Sequence[Species]
-    rate_law: Callable
-    equations: list[Equation]
-
-    def __init__(
-        self,
-        *,
-        reactants: Sequence[Symbol] = (),
-        products: Sequence[Symbol] = (),
-        rate_law: Callable,
-    ):
-        self.reactants = [Species.from_symbol(r) for r in reactants]
-        self.products = [Species.from_symbol(p) for p in products]
-        self.rate_law = rate_law
-        self.equations = list(self.yield_equations())
-
-    def __get__(self, obj, cls):
-        if obj is None:
-            return self
-
-        try:
-            return obj.__dict__[self.name]
-        except KeyError:
-            reactants = [
-                v.stoichiometry * getattr(obj, v.variable.name) for v in self.reactants
-            ]
-            products = [
-                v.stoichiometry * getattr(obj, v.variable.name) for v in self.products
-            ]
-            rate_law = self.rate_law
-            copy = self.__class__(
-                reactants=reactants,
-                products=products,
-                rate_law=rate_law,
-            )
-            obj.__dict__[self.name] = copy
-            return copy
-
-    def yield_equations(self) -> Iterator[Equation]:
-        rate = self.rate_law(*self.reactants)
-        species_stoich: dict[Variable, float] = defaultdict(float)
-        for r in self.reactants:
-            species_stoich[r.variable] -= r.stoichiometry
-        for p in self.products:
-            species_stoich[p.variable] += p.stoichiometry
-
-        for s, st in species_stoich.items():
-            yield s.derive() << st * rate
-
-
 @dataclass(frozen=True)
-class Species:
+class Species(Owned):
     variable: Variable
     stoichiometry: float
+
+    def __str__(self):
+        return f"{self.stoichiometry} {self.variable}"
 
     @classmethod
     def from_symbol(cls, x: Symbol) -> Self:
@@ -92,6 +43,49 @@ class Species:
         return cls(variable=species, stoichiometry=stoichiometry)
 
 
+class Reaction(Owned):
+    reactants: Sequence[Species]
+    products: Sequence[Species]
+    rate_law: Callable
+    equations: list[Equation]
+
+    def __init__(
+        self,
+        *,
+        reactants: Sequence[Symbol] = (),
+        products: Sequence[Symbol] = (),
+        rate_law: Callable,
+    ):
+        self.reactants = [Species.from_symbol(r) for r in reactants]
+        self.products = [Species.from_symbol(p) for p in products]
+        self.rate_law = rate_law
+        self.equations = list(self.yield_equations())
+
+    def _copy_from(self, parent: System):
+        return self.__class__(
+            reactants=[
+                v.stoichiometry * getattr(parent, v.variable.name)
+                for v in self.reactants
+            ],
+            products=[
+                v.stoichiometry * getattr(parent, v.variable.name)
+                for v in self.products
+            ],
+            rate_law=self.rate_law,
+        )
+
+    def yield_equations(self) -> Iterator[Equation]:
+        rate = self.rate_law(*self.reactants)
+        species_stoich: dict[Variable, float] = defaultdict(float)
+        for r in self.reactants:
+            species_stoich[r.variable] -= r.stoichiometry
+        for p in self.products:
+            species_stoich[p.variable] += p.stoichiometry
+
+        for s, st in species_stoich.items():
+            yield s.derive() << st * rate
+
+
 class MassAction(Reaction):
     def __init__(
         self,
@@ -111,27 +105,19 @@ class MassAction(Reaction):
             rate *= r.variable**r.stoichiometry
         return rate
 
-    def __get__(self, obj, cls):
-        if obj is None:
-            return self
+    def _copy_from(self, parent: System):
+        rate = self.rate
+        if isinstance(rate, Owned):
+            rate = getattr(parent, rate.name, rate)
 
-        try:
-            return obj.__dict__[self.name]
-        except KeyError:
-            reactants = [
-                v.stoichiometry * getattr(obj, v.variable.name) for v in self.reactants
-            ]
-            products = [
-                v.stoichiometry * getattr(obj, v.variable.name) for v in self.products
-            ]
-            try:
-                rate = getattr(obj, self.rate.name, self.rate)
-            except AttributeError:
-                rate = self.rate
-            copy = self.__class__(
-                reactants=reactants,
-                products=products,
-                rate=rate,
-            )
-            obj.__dict__[self.name] = copy
-            return copy
+        return self.__class__(
+            reactants=[
+                v.stoichiometry * getattr(parent, v.variable.name)
+                for v in self.reactants
+            ],
+            products=[
+                v.stoichiometry * getattr(parent, v.variable.name)
+                for v in self.products
+            ],
+            rate=rate,
+        )
