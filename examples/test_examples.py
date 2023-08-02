274af91a0a9b2deb2a4dd9219c30c6e9b13cf690
Mauro Silberberg <maurosilber@gmail.com>
2023-08-02 11:51:23 -0300

    Add test for examples

---
+++ examples/test_examples.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from runpy import run_path
+
+import matplotlib.pyplot as plt
+from pytest import mark
+
+path = Path("examples")
+examples = [str(p) for p in path.glob("*.py") if not p.name.startswith("test_")]
+
+
+@mark.parametrize("example", examples)
+def test_example(example, monkeypatch):
+    monkeypatch.setattr(plt, "show", noop)
+    run_path(example)
+
+
+def noop(*args, **kwargs):
+    pass
