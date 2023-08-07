7954924a4c7c1045cab14d8972e79dad040ffc2d
Mauro Silberberg <maurosilber@gmail.com>
2023-08-07 10:56:52 -0300

    Update test for resolution of initials

--- src/poincare/tests/simulator/test_initials.py
+++ src/poincare/tests/simulator/test_initials.py
@@ -1,7 +1,8 @@
 from pytest import raises
+from symbolite import Symbol
 
-from ... import Constant, Parameter, System, Variable
 from ...simulator import Simulator
+from ...types import Constant, Derivative, Number, Parameter, System, Variable
 
 
 class Model(System):
@@ -12,82 +13,85 @@ class Model(System):
     k4 = Constant(default=2 * k1)
     p = Parameter(default=k3 + 1)
     x = Variable(initial=k3 + k4)
+    eq = x.derive() << p * x
+
+
+def assert_initials(
+    system: System | type[System],
+    values: dict[Constant | Parameter | Variable | Derivative, Number | Symbol],
+    *,
+    expected_parameters: dict[Parameter, float],
+    expected_variables: dict[Variable, float],
+):
+    sim = Simulator(system)
+    problem = sim.create_problem(values)
+    assert dict(zip(sim.compiled.parameter_names, problem.p)) == {
+        str(k): v for k, v in expected_parameters.items()
+    }
+    assert dict(zip(sim.compiled.variable_names, problem.y)) == {
+        str(k): v for k, v in expected_variables.items()
+    }
 
 
 def test_default_initials():
-    assert Simulator(Model)._resolve_initials() == {
-        Model.k0: 0,
-        Model.k1: 0,
-        Model.k2: 0,
-        Model.k3: 0,
-        Model.k4: 0,
-        Model.p: 1,
-        Model.x: 0,
-    }
+    assert_initials(
+        Model,
+        values={},
+        expected_parameters={Model.p: 1},
+        expected_variables={Model.x: 0},
+    )
 
 
 def test_override_initials():
-    assert Simulator(Model)._resolve_initials(values={Model.k3: 1}) == {
-        Model.k0: 0,
-        Model.k1: 0,
-        Model.k2: 0,
-        Model.k3: 1,
-        Model.k4: 0,
-        Model.p: 2,
-        Model.x: 1,
-    }
-    assert Simulator(Model)._resolve_initials(values={Model.k2: 1}) == {
-        Model.k0: 0,
-        Model.k1: 0,
-        Model.k2: 1,
-        Model.k3: 1,
-        Model.k4: 0,
-        Model.p: 2,
-        Model.x: 1,
-    }
-    assert Simulator(Model)._resolve_initials(values={Model.k1: 1}) == {
-        Model.k0: 0,
-        Model.k1: 1,
-        Model.k2: 1,
-        Model.k3: 2,
-        Model.k4: 2,
-        Model.p: 3,
-        Model.x: 4,
-    }
-    assert Simulator(Model)._resolve_initials(values={Model.k0: 1}) == {
-        Model.k0: 1,
-        Model.k1: 1,
-        Model.k2: 1,
-        Model.k3: 2,
-        Model.k4: 2,
-        Model.p: 3,
-        Model.x: 4,
-    }
-    assert Simulator(Model)._resolve_initials(values={Model.k0: 1, Model.k2: 2}) == {
-        Model.k0: 1,
-        Model.k1: 1,
-        Model.k2: 2,
-        Model.k3: 3,
-        Model.k4: 2,
-        Model.p: 4,
-        Model.x: 5,
-    }
+    assert_initials(
+        Model,
+        values={Model.k3: 1},
+        expected_parameters={Model.p: 2},
+        expected_variables={Model.x: 1},
+    )
+    assert_initials(
+        Model,
+        values={Model.k2: 1},
+        expected_parameters={Model.p: 2},
+        expected_variables={
+            Model.x: 1,
+        },
+    )
+    assert_initials(
+        Model,
+        values={Model.k1: 1},
+        expected_parameters={Model.p: 3},
+        expected_variables={
+            Model.x: 4,
+        },
+    )
+    assert_initials(
+        Model,
+        values={Model.k0: 1},
+        expected_parameters={Model.p: 3},
+        expected_variables={
+            Model.x: 4,
+        },
+    )
+    assert_initials(
+        Model,
+        values={Model.k0: 1, Model.k2: 2},
+        expected_parameters={Model.p: 4},
+        expected_variables={
+            Model.x: 5,
+        },
+    )
 
 
 def test_override_with_constants():
-    assert Simulator(Model)._resolve_initials(
-        values={Model.k0: 2, Model.k1: 1, Model.k2: Model.k0}
-    ) == {
-        Model.k0: 2,
-        Model.k1: 1,
-        Model.k2: 2,
-        Model.k3: 3,
-        Model.k4: 2,
-        Model.p: 4,
-        Model.x: 5,
-    }
+    assert_initials(
+        Model,
+        values={Model.k0: 2, Model.k1: 1, Model.k2: Model.k0},
+        expected_parameters={Model.p: 4},
+        expected_variables={Model.x: 5},
+    )
 
 
 def test_cyclic_initials():
     with raises(RecursionError, match="cyclic"):
-        Simulator(Model)._resolve_initials(values={Model.k0: Model.k2})
+        Simulator(Model).create_problem(values={Model.k0: Model.k2})
