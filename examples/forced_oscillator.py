de8ce1cdcf38b9a2825aca78b35f374c596df86e
Mauro Silberberg <maurosilber@gmail.com>
2023-08-09 14:25:57 -0300

    Update forced oscillator example with additional funcional form

--- examples/forced_oscillator.py
+++ examples/forced_oscillator.py
@@ -4,17 +4,30 @@ from poincare import Derivative, Parameter, System, Variable, assign, initial
 from poincare.simulator import Simulator
 from symbolite import scalar
 
+t = System.simulation_time
+
 
 class Oscillator(System):
     x: Variable = initial(default=1)
     vx: Derivative = x.derive(initial=0)
-    spring_constant: Parameter = assign(default=1)
-    F: Parameter = assign(default=scalar.cos(System.simulation_time))
-    spring = vx.derive() << -spring_constant * x + F
+    phase: Parameter = assign(default=0)
+    F: Parameter = assign(default=scalar.cos(t + phase))
+    spring = vx.derive() << -x + F
 
 
 if __name__ == "__main__":
     sim = Simulator(Oscillator)
     result = sim.solve(times=np.linspace(0, 50, 1000))
-    result["x"].plot()
+    result["x"].rename("cos(t)").plot()
+
+    # No recompilation necessary to change parameter value
+    result = sim.solve(times=np.linspace(0, 50, 1000), values={Oscillator.phase: -1.3})
+    result["x"].rename("cos(t-1.3)").plot()
+
+    # To change functional form, must be recompiled
+    sim = Simulator(Oscillator(F=scalar.sin(t)))
+    result = sim.solve(times=np.linspace(0, 50, 1000))
+    result["x"].rename("sin(t)").plot()
+
+    plt.legend()
     plt.show()
