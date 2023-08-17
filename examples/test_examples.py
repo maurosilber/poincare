938af8107f4d4df8f07512e86c9179ab2f6ea1a4
Mauro Silberberg <maurosilber@gmail.com>
2023-08-17 19:20:02 -0300

    Fix test_examples: it was not running the __main__ section

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -11,7 +11,7 @@ examples = [str(p) for p in path.glob("*.py") if not p.name.startswith("test_")]
 @mark.parametrize("example", examples)
 def test_example(example, monkeypatch):
     monkeypatch.setattr(plt, "show", noop)
-    run_path(example)
+    run_path(example, run_name="__main__")
 
 
 def noop(*args, **kwargs):
