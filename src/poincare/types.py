a337ae5e1087d3d275d30d21d30a082fe1eaade6
Mauro Silberberg <maurosilber@gmail.com>
2023-08-18 10:27:02 -0300

    Move units-related code to units module

--- src/poincare/types.py
+++ src/poincare/types.py
@@ -8,81 +8,28 @@ import pint
 from symbolite import Scalar, Symbol
 from symbolite import abstract as libabstract
 from symbolite.core import evaluate, substitute
-from symbolite.impl import libstd
 from typing_extensions import Self, dataclass_transform
 
+from . import units
 from ._node import Node, NodeMapper
-from .units import register_with_pint
 
 T = TypeVar("T")
 
 
-register_with_pint(Symbol)
+units.register_with_pint(Symbol)
 
 
-class EvalUnitError(Exception):
-    pass
-
-
-def try_eval_units(value):
-    try:
-        return evaluate(value, libsl=libstd)
-    except EvalUnitError:
-        return None
-
-
-def check_equations_units(lhs: Derivative, rhs):
-    order = 0
-    if (value := lhs.variable.initial) is None:
-        # Maybe a derivative has a unit already assigned
-        for order, der in lhs.variable.derivatives.items():
-            if (value := der.initial) is not None:
-                break
-        else:
-            # No unit assigned. Only check that rhs is consistent.
-            try_eval_units(rhs)
-            return
-
-    value = try_eval_units(value)
-    rhs = try_eval_units(rhs)
-    if rhs is None:
-        return
-    if isinstance(value, pint.Quantity):
-        time = value._REGISTRY.s
-    elif isinstance(rhs, pint.Quantity):
-        time = rhs._REGISTRY.s
-    else:
-        return
-
-    value / time ** (lhs.order - order) - rhs  # check units
-    return
-
-
-def check_units(var, value):
-    lhs = try_eval_units(var)
-    rhs = try_eval_units(value)
-
-    if lhs is not None and rhs is not None:
-        lhs - rhs  # must have same units
-        return
-
-
-def check_derivative_units(derivative: Derivative, value):
-    check_units(derivative, value)
-    check_equations_units(derivative, value)
-
-
-@register_with_pint
+@units.register_with_pint
 class Constant(Node, Scalar):
     def __init__(self, *, default: Initial | None):
         self.default = default
-        check_units(self, default)
+        units.check_units(self, default)
 
     def eval(self, libsl=None):
         if libsl is libabstract:
             return self
         elif self.default is None:
-            raise EvalUnitError
+            raise units.EvalUnitError
         else:
             return evaluate(self.default, libsl)
 
@@ -104,7 +51,7 @@ class Constant(Node, Scalar):
         elif isinstance(value, Initial):
             # Get or create instance with getattr
             constant: Constant = getattr(obj, self.name)
-            check_units(constant, value)
+            units.check_units(constant, value)
             constant.default = value
         else:
             raise TypeError(f"unexpected type {type(value)} for {self.name}")
@@ -133,19 +80,19 @@ def _assign_equation_order(
     variable.equation_order = order
 
 
-@register_with_pint
+@units.register_with_pint
 class Parameter(Node, Scalar):
     equation_order: int = 0
 
     def __init__(self, *, default: Initial | Symbol | None):
         self.default = default
-        check_units(self, default)
+        units.check_units(self, default)
 
     def eval(self, libsl=None):
         if libsl is libabstract:
             return self
         elif self.default is None:
-            raise EvalUnitError
+            raise units.EvalUnitError
         else:
             return evaluate(self.default, libsl)
 
@@ -159,7 +106,7 @@ class Parameter(Node, Scalar):
             # Get or create instance with getattr
             # Update initial value
             variable: Self = getattr(obj, self.name)
-            check_units(variable, value)
+            units.check_units(variable, value)
             variable.default = value
         else:
             raise TypeError(f"unexpected type {type(value)} for {self.name}")
@@ -174,7 +121,7 @@ class Parameter(Node, Scalar):
         return self.default == other.default and super().__eq__(other)
 
 
-@register_with_pint
+@units.register_with_pint
 class Variable(Node, Scalar):
     initial: Initial | None
     derivatives: dict[int, Derivative]
@@ -183,13 +130,13 @@ class Variable(Node, Scalar):
     def __init__(self, *, initial: Initial | None):
         self.initial = initial
         self.derivatives = {}
-        check_units(self, initial)
+        units.check_units(self, initial)
 
     def eval(self, libsl=None):
         if libsl is libabstract:
             return self
         elif self.initial is None:
-            raise EvalUnitError
+            raise units.EvalUnitError
         else:
             return evaluate(self.initial, libsl)
 
@@ -234,7 +181,7 @@ class Variable(Node, Scalar):
             elif isinstance(value, Initial):
                 # Create internal variable and update its initial
                 variable: Variable = getattr(obj, self.name)
-                check_units(variable, value)
+                units.check_units(variable, value)
                 variable.initial = value
         else:
             # Variable already assigned (by a Derivative)
@@ -256,7 +203,7 @@ class Variable(Node, Scalar):
         return self.initial == other.initial and super().__eq__(other)
 
 
-@register_with_pint
+@units.register_with_pint
 class Derivative(Node, Symbol):
     def __new__(
         cls,
@@ -287,14 +234,14 @@ class Derivative(Node, Symbol):
         self.variable = variable
         self.order = order
         self.initial = initial
-        check_derivative_units(self, initial)
+        units.check_derivative_units(self, initial)
         self.variable.derivatives[order] = self
 
     def eval(self, libsl=None):
         if libsl is libabstract:
             return self
         elif self.initial is None:
-            raise EvalUnitError
+            raise units.EvalUnitError
         else:
             return evaluate(self.initial, libsl)
 
@@ -323,7 +270,7 @@ class Derivative(Node, Symbol):
                 # this creates an internal variable, which will be an error
                 # if it is going to be assigned to an outside variable
                 derivative: Derivative = getattr(obj, self.name)
-                check_derivative_units(derivative, value)
+                units.check_derivative_units(derivative, value)
                 derivative.initial = value
         else:
             # derivative has been assigned -> variable has been assigned
@@ -334,7 +281,7 @@ class Derivative(Node, Symbol):
                         "derivative corresponds to an external variable, cannot change its initial condition here."
                     )
                 else:
-                    check_derivative_units(derivative, value)
+                    units.check_derivative_units(derivative, value)
                     derivative.initial = value
             elif isinstance(value, Derivative) and value is not derivative:
                 raise TypeError("assigned wrong derivative")
@@ -374,7 +321,7 @@ class Equation(Node):
     rhs: Initial | Symbol
 
     def __post_init__(self):
-        check_equations_units(self.lhs, self.rhs)
+        units.check_equations_units(self.lhs, self.rhs)
 
     def _copy_from(self, parent: System):
         variable = getattr(parent, self.lhs.variable.name)
@@ -436,7 +383,7 @@ def initial(*, default: Initial | None = None, init: bool = True) -> Variable:
     return Variable(initial=default)
 
 
-@register_with_pint
+@units.register_with_pint
 class Time(Parameter):
     def _copy_from(self, parent: System):
         raise NotImplementedError
--- src/poincare/units.py
+++ src/poincare/units.py
@@ -1,6 +1,14 @@
-from typing import TypeVar
+from __future__ import annotations
 
+from typing import TYPE_CHECKING, TypeVar
+
+import pint
 from pint.compat import fully_qualified_name, upcast_type_map
+from symbolite.core import evaluate
+from symbolite.impl import libstd
+
+if TYPE_CHECKING:
+    from .types import Derivative
 
 T = TypeVar("T", bound=type)
 
@@ -12,3 +20,55 @@ def register_with_pint(cls: T) -> T:
     """
     upcast_type_map[fully_qualified_name(cls)] = cls
     return cls
+
+
+class EvalUnitError(Exception):
+    pass
+
+
+def try_eval_units(value):
+    try:
+        return evaluate(value, libsl=libstd)
+    except EvalUnitError:
+        return None
+
+
+def check_equations_units(lhs: Derivative, rhs):
+    order = 0
+    if (value := lhs.variable.initial) is None:
+        # Maybe a derivative has a unit already assigned
+        for order, der in lhs.variable.derivatives.items():
+            if (value := der.initial) is not None:
+                break
+        else:
+            # No unit assigned. Only check that rhs is consistent.
+            try_eval_units(rhs)
+            return
+
+    value = try_eval_units(value)
+    rhs = try_eval_units(rhs)
+    if rhs is None:
+        return
+    if isinstance(value, pint.Quantity):
+        time = value._REGISTRY.s
+    elif isinstance(rhs, pint.Quantity):
+        time = rhs._REGISTRY.s
+    else:
+        return
+
+    value / time ** (lhs.order - order) - rhs  # check units
+    return
+
+
+def check_units(var, value):
+    lhs = try_eval_units(var)
+    rhs = try_eval_units(value)
+
+    if lhs is not None and rhs is not None:
+        lhs - rhs  # must have same units
+        return
+
+
+def check_derivative_units(derivative: Derivative, value):
+    check_units(derivative, value)
+    check_equations_units(derivative, value)
