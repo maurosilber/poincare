9a1d76a7502791a550ea55b9d22e4ae5c1d89ec3
Mauro Silberberg <maurosilber@gmail.com>
2023-08-22 22:19:48 -0300

    Test for derivative and equations outside class or in subclass

---
+++ src/poincare/tests/test_subclass.py
@@ -0,0 +1,37 @@
+from ..types import System, Variable, initial
+
+
+def test_derivative_outside_class():
+    class Base(System):
+        x: Variable = initial(default=0)
+
+    Base.x.derive(initial=0)
+    assert 1 not in Base.x.derivatives
+
+
+def test_derivative_in_subclass():
+    class Base(System):
+        x: Variable = initial(default=0)
+
+    class Extended(Base):
+        vx = Base.x.derive(initial=0)
+
+    assert 1 not in Base.x.derivatives
+
+
+def test_equation_outside_class():
+    class Base(System):
+        x: Variable = initial(default=0)
+
+    Base.x.derive() << 1
+    assert Base.x.equation_order == 0
+
+
+def test_equation_in_subclass():
+    class Base(System):
+        x: Variable = initial(default=0)
+
+    class Extended(Base):
+        eq = Base.x.derive() << 1
+
+    assert Base.x.equation_order == 0
